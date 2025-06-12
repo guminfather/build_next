2cf091659945b8a11e23f04055955be19c725ed5
김은정 <kejgogo78@gmail.com>
2025-06-13 07:00:38 +0900

    비밀번호 찾기 보내는 메일주소 변경

--- pages/api/send-email.ts
+++ pages/api/send-email.ts
@@ -18,18 +18,28 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       port: 587,               // 포트
       secure: false,           // true일 경우 SSL 사용 (465), false일 경우 TLS 사용 (587)
       auth: {
-        user: process.env.EMAIL_USER, // Gmail 주소 (관리자)
-        pass: process.env.EMAIL_PASS, // Gmail 앱 비밀번호 (보통 16자리) 
+        //user: process.env.EMAIL_USER, // Gmail 주소 (관리자)
+        //pass: process.env.EMAIL_PASS, // Gmail 앱 비밀번호 (보통 16자리) 
+        user: "kejgogo78@gmail.com", // Gmail 주소 (관리자)
+        pass: "smwbrrzvjvfldsyv", // Gmail 앱 비밀번호 (보통 16자리) 
+        
       },
     });
 
+    await transporter.sendMail({
+      from: `KEES 관리자" <kejgogo78@gmail.com>`,
+      to: `"${name}" <${email}>`, // 수신자 이메일
+      subject: '[KEES 서비스] 임시 비밀번호가 발급되었습니다. (** 로그인 후 꼭 변경해주세요)',
+      html: message,    // HTML 본문 사용
+    });
+    /*
     await transporter.sendMail({
       from: `KEES 관리자" <${process.env.EMAIL_USER}>`,
       to: `"${name}" <${email}>`, // 수신자 이메일
       subject: '[KEES 서비스] 임시 비밀번호가 발급되었습니다. (** 로그인 후 꼭 변경해주세요)',
       // text: message, // 제거
       html: message,    // HTML 본문 사용
-    });
+    });*/
 
     res.status(200).json({ message: '메일 전송 성공' });
   } catch (error) {
